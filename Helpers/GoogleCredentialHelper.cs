71179bad2c870a3552c8da6507273a332c085ae9
Aren Chen <gucci313031@gmail.com>
2022-08-23 14:34:36 +0800

    First Release

---
+++ Helpers/GoogleCredentialHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using Google.Apis.Auth.OAuth2;
+
+namespace Netcorext.Extensions.Google.Apis.Helpers;
+
+public static class GoogleCredentialHelper
+{
+    public static GoogleCredential FromBase64String(string base64)
+    {
+        var bytes = Convert.FromBase64String(base64);
+        var json = Encoding.UTF8.GetString(bytes);
+        return GoogleCredential.FromJson(json);
+    }
+}
